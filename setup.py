d69c33fa1fc7fdbff7ebc839c1cdd30856d35068
John Fike <johnfike223@gmail.com>
2024-04-08 18:35:55 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from libcolonyfind import __version__
+
+setup(
+    name='libcolonyfind',
+    version=__version__,
+
+    url='https://github.com/msudesigncpr/libcolonyfind',
+    author='John Fike',
+    author_email='colonypicker@gmail.com',
+
+    py_modules=['libcolonyfind'],
+)
