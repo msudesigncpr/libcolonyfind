5f0b4fa01224e40307e27a64ae9a569dcbcd33fa
John Fike <johnfike223@gmail.com>
2024-04-13 16:17:06 -0600

    update constants doclines

--- src/libcolonyfind/constants.py
+++ src/libcolonyfind/constants.py
@@ -1,12 +1,38 @@
 MIN_COLONY_DISTANCE = 0.01 
+"""
+Used by remove_unsampleable_colonies()
+Defines the minimum distance allowed between the edges of two colonies
+Neither colony will be sampled from if they are closer than this distance
+This distance is in fractions of an image. Hopefully I remember to change that...
+"""
+
 MIN_COLONY_RADIUS = 0.004 #FIXME THIS MAY BE TOO SMALL. I DO NOT KNOW. I NEED TO FIGURE IT OUT!
+"""
+Used by remove_unsampleable_colonies()
+Defines the minimum radius for a colony to be sampleable. 
+Kind of just a potshot number right now. 
+This distance is in fractions of an image. Hopefully I remember to change that...
+"""
+
 
 PETRI_DISH_ROI = 0.25
+"""
+Used by remove_unsampleable_colonies()
+Defines distance from center of image colonies can be to be considered sampleable
+This distance is in fractions of an image. Hopefully I remember to change that...
+"""
 
 IMG_HEIGHT = 2448
 IMG_WIDTH = 3264
+"""
+Image sizes are for the camera in the BOM: https://github.com/msudesigncpr/datasheets/blob/tonic/BOM.md
+"""
 
-CFU_WIN_PATH = '\\\\wsl.localhost/Debian/home/colon/OpenCFU' # DO NOT CHANGE YOU'LL BREAK SHIT
+CFU_WIN_PATH = '\\\\wsl.localhost/Debian/home/colon/OpenCFU'
+"""
+Path to wherever CFU is installed in WSL. Yes those four backslashes are necesary. No I don't like it either. 
+"""
+
 
 WELLS =['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12',
         'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12',
@@ -16,10 +42,22 @@ WELLS =['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12
         'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',                    
         'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10', 'G11', 'G12',
         'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'H11', 'H12']
+"""
+Used by annotate_images() to write the well each colony is destined for on the image
+"""
 
-# effectivley just conversion factors for going from mmm to pixels
-GSD_X = 174.45 # mm per pixel in the x direction
-GSD_Y = 125.84 # mm per pixel in the y direction
 
+GSD_X = 174.45 
+"""
+mm per pixel in the x direction
+"""
 
-# MIN_COLONY_RADIUS = 1/125.84 * (1/IMG_WIDTH)
+GSD_Y = 125.84  
+"""
+mm per pixel in the y direction
+"""
+
+"""
+effectivley just conversion factors for going from mmm to pixels
+"""
+
